c7d0eab6645d00db8deae2626db90f804b11d93a
soyuz43 <kebekad673@proton.me>
2025-01-21 15:46:47 -0600

    Fixed bugs, add event listners, and fixed msinnamed iterators

--- scripts/Pairings.js
+++ scripts/Pairings.js
@@ -1,33 +1,34 @@
-import { getChildren } from "./database.js"
+import { getChildren, getCelebrities } from "./database.js";
 
-const kids = getChildren()
-const celebrities = getCelebrities()
 
-const findCelebrityMatch = (kidObject, celebrityArray) => {
-    let celebrity = null
-
-    for (const celebrity of celebrtyArray) {
-
+const findCelebrityMatch = (kid, celebrities) => {
+    for (const star of celebrities) {
+        if (kid.celebrityId === star.id) {
+            return star;
+        }
     }
-
-    return celebritiy
-}
+    return null;
+};
 
 export const Pairings = () => {
-    let html = ""
-    html = "<ul>"
+    const kids = getChildren();
+    const celebrities = getCelebrities();
+    let html = "<ul>";
 
     for (const kid of kids) {
-        const kidsStar = findCelebrityMatch(kid, celebrities)
-        html += `
-            <li>
-                ${kid.name} will be making memories with ${kidsStar.name}, a ${kidsStar.Sport} star, by ${kid.wish}
-            </li>
-        `
+        const kidsStar = findCelebrityMatch(kid, celebrities);
+        if (kidsStar) {
+            html += `<li>
+                        ${kid.name} will be making memories with ${kidsStar.name}, a ${kidsStar.sport} star, by ${kid.wish}
+                    </li>`;
+        } else {
+            html += `<li>
+                        ${kid.name} doesn't have a matching star.
+                    </li>`;
+        }
     }
 
-    html += "</ul>"
+    html += "</ul>";
 
-    return html
-}
-
+    return html;
+};
